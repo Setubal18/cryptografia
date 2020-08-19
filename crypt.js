1301cfafff293dae51ad3cb40475e0b2f15d147a
André F. Bispo <andrefer483@gmail.com>
2020-08-18 21:30:38 -0300

    Descryptografy was added, but not done

--- crypt.js
+++ crypt.js
@@ -1,21 +1,26 @@
+var keysOfOperation = {
+    multiplied: [],
+    divided: []
+}
+
 function transfromStringToList(textToSplit) {
     return textToSplit.split('')
 }
 
-function createString(text) {
+function encrypt(text) {
     const textToASCII = transfromStringToList(text)
-        .reduce(function(accumulator, currentValue) {
-        
+        .reduce(function (accumulator, currentValue) {
+
             const valueInText = transformLetterToASCII(currentValue)
 
             var valueMultiplied = multiply(valueInText)
 
             if (valueMultiplied.includes('.')) {
-                valueMultiplied = tranformCodefractionated(valueMultiplied)
+                // valueMultiplied = tranformCodeFractionated(valueMultiplied)
             }
-            
+
             if (valueMultiplied.length < 3) {
-                valueMultiplied = `${getHexNumber(1)}${valueMultiplied}`
+                // valueMultiplied = `${getHash(1)}${valueMultiplied}`
             }
 
             console.log(valueMultiplied)
@@ -30,45 +35,86 @@ function transformLetterToASCII(currentValue) {
 }
 
 function multiply(currentValue) {
-    const valueMultiplied = ((currentValue * 2) <= 255) 
-        ? currentValue * 2
-        : currentValue / 2
-    return String(valueMultiplied)
+    var valueMultiplied = '', multiplied = false
+
+    if ((currentValue * 2) <= 255) {
+        valueMultiplied = String(currentValue * 2)
+        multiplied = true
+    }
+    else {
+        SvalueMultiplied = String(currentValue / 2)
+    }
+
+    if (multiplied === true) {
+        console.log('Entrou1111111')
+        var hash = checkExistInKeysOfOperation(keysOfOperation.multiplied, keysOfOperation.divided, hash)
+        addHashInKeysOfOperation(keysOfOperation.multiplied, hash)
+    }
+    else {
+        console.log('Entrou22222222')
+        var hash = checkExistInKeysOfOperation(keysOfOperation.multiplied, keysOfOperation.divided, hash)
+        addHashInKeysOfOperation(keysOfOperation.divided, hash)
+    }
+
+    const valueWithHash = valueMultiplied += hash
+
+    return valueWithHash
 }
 
-function tranformCodefractionated(valueMultiplied) {
-    return valueMultiplied.replace('.', `0x${getHexNumber(2)}`);
+function addHashInKeysOfOperation(value, hash) {
+    if (value.length <= 7) {
+        value.push(hash)
+    }
 }
 
-function getHexNumber(counter) {
+function checkExistInKeysOfOperation(value, value2, hash) {
+    while (value.includes(hash) || value2.includes(hash) || hash === undefined) {
+        hash = getHash(1, true)
+    }
+    return hash
+}
+
+
+function tranformCodeFractionated(valueMultiplied) {
+    return valueMultiplied.replace('.', `0x${getHash(2)}`);
+}
+
+function stringsForHash(counter, stringType = false) {
+    if (counter === 1) {
+        return stringType === false
+            ? 'abcdefghijklmnopqrstuvwxyz'
+            : '!@#$%&*()_-=/+'
+    }
+    else if (counter === 2) {
+        return '0123456789abcdef'
+    }
+}
+
+function getHash(counter, stringType) {
     let string = '', random = ''
-
-    // const letterNumber = '0123456789abcdef'
-    // const alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-    const strings = counter === 1
-        ? 'abcdefghijklmnopqrstuvwxyz'
-        : '0123456789abcdef'
+    const strings = stringsForHash(counter, stringType)
 
     while (random.length < counter) {
         string = Math.round(Math.random() * strings.length);
         random += strings.substring(string, string + 1);
     }
 
-    if (random == '0x') {
-        getHexNumber(counter)
-    }
+    // if (random == '0x') {getHash(counter)}
 
     return random
 }
 
+
+console.log(keysOfOperation)
+
 const text = 'Samuel é'//'Desculpe, só trabalho com casadas'
 
-const textEncrypted = createString(text)
+const textEncrypted = encrypt(text)
 
 console.log(textEncrypted)
 
-
+// console.log(multiplied)
+// console.log(divided)
 
 
 
@@ -77,15 +123,17 @@ function descrypt(textEncrypted) {
     const textWithoutPoints = descryptPoints(textEncrypted)
     console.log(textWithoutPoints)
 
-    const removeL = removeLetters(textWithoutPoints)
-    console.log(removeL)
+    const lettersRemoved = removeLetters(textWithoutPoints)
+    console.log(lettersRemoved)
+
+    const codeChar = getCode(lettersRemoved)
+    console.log(codeChar)
 }
 
-function descryptPoints(textEncrypted) {
+function descryptPoints(removeL) {
     var newText = ''
 
     const pointsCodes = searchPoints(textEncrypted)
-    console.log(pointsCodes)
 
     for (char of pointsCodes) {
         newText = textEncrypted.replace(pointsCodes, '.')
@@ -94,7 +142,7 @@ function descryptPoints(textEncrypted) {
 }
 
 function searchPoints(textEncrypted) {
-    
+
     if (textEncrypted.includes('0x')) {
         var accumulator = [], codes = ''
 
@@ -130,8 +178,7 @@ function removeLetters(textWithoutPoints) {
 
     var newText = ''
     for (letter of letters) {
-        console.log(letter)
-        newText =textWithoutPoints.replace(letter, '0')
+        newText = textWithoutPoints.replace(letter, '0')
     }
     return newText
 }
@@ -151,149 +198,28 @@ function findLetters(textWithoutPoints) {
     return listOfLetters
 }
 
-
-
-descrypt(textEncrypted)
-
-
-
-
-
-
-
-
-
-
-
-
-// var request = function(url) {
-//     fetch(url, {
-//         cache: 'reload' // definir o tipo de cache
-//     }).then(function(response) {
-//         if ( response.ok ) {
-//             console.log(response)
-//             // texto simples
-//             return response.text();
-//             // para transformar o `json` que é `string` para um `object`
-//             // javascript use `response.json()` que o próximo `then` retornará um `object`
-//         }
-//         // em caso de resposta "opaca" ou outros erros
-//         throw new Error('Fetch error, status code: ' + response.status);
-//     }).then(function(text) {
-//         // buscar elemento (div)
-//         var el = document.getElementById('container');
-//         // adicionar resultado (objeto para string)
-//         el.innerHTML = el.innerHTML += '<br>' + text;
-//     }).catch(function(error) {
-//         // caso haja um erro... tratar aqui
-//     });
-
-// };
-
-// console.log(request('file:///C:/Users/andre/Desktop/cryptografia/file.txt'))
-
-
-// function readTextFile(file)
-// {
-//     var rawFile = new XMLHttpRequest();
-//     rawFile.open("GET", file, false);
-//     rawFile.onreadystatechange = function ()
-//     {
-//         if(rawFile.readyState === 4)
-//         {
-//             if(rawFile.status === 200 || rawFile.status == 0)
-//             {
-//                 var allText = rawFile.responseText;
-//                 console(allText)
-//             }
-//         }
-//     }
-//     rawFile.send(null);
-// }
-
-// readTextFile("file:///C:/Users/andre/Desktop/cryptografia/file.txt");
-
-// fetch('file:///C:/Users/andre/Desktop/cryptografia/file.txt')
-//   .then(response => response.text())
-//   .then(text => console.log(text))
-
-// var openFile = function(event) {
-//     var input = event.target;
-
-//     var reader = new FileReader();
-//     reader.onload = function(){
-//       var text = reader.result;
-//       var node = document.getElementById('output');
-//       node.innerText = text;
-//       console.log(reader.result.substring(0, 200));
-//     };
-//     reader.readAsText(input.files[0]);
-//   };
-
-// openFile('file:///C:/Users/andre/Desktop/cryptografia/file.txt')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function tranformCodefractionated(valueMultiplied) {
-//     // const random = getHexNumber()
-
-//     // console.log(historyToHexNumber(valueMultiplied, random))
-
-//     return valueMultiplied.replace('.', '0x46');
-// }
-
-// function getHexNumber() {
-//     let string = '', random = ''
-//     // const letterNumber = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']
-//     const letterNumber = '0123456789abcdef'
-
-//     for (let count = 0; count < 2; count++) {
-//         string = Math.round(Math.random() * letterNumber.length);
-//         random += letterNumber.substring(string, string + 1);
-//     }
-//     return random
-// }
-
-// function historyToHexNumber(valueMultiplied, random) {
-//     valueMultiplied = `key-${valueMultiplied}`
-
-//     console.log('asdf',valueMultiplied, random)
-
-//     // history.push({
-//     //     valueMultiplied: random
-//     // })
-// }
-
-// A = 065 --- 65 * 2= 130 = é
+function getCode(lettersRemoved) {
+    var listOfCodes = [], aux = ''
+    let { length, [length - 1]: lastElement } = listOfCodes
+
+    for (let str of lettersRemoved) {
+        if (aux.length === 3) {
+            listOfCodes.push(aux)
+            aux = ''
+        }
+
+        if (listOfCodes.length >= 1) {
+            if (str === '.' || listOfCodes[listOfCodes.length - 1].length === 4) {
+                listOfCodes[listOfCodes.length - 1] += str
+                str = ''
+            }
+        }
+
+        aux += str
+    }
+    return listOfCodes
+}
+
+
+
+// descrypt(textEncrypted)
